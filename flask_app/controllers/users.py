3da1d4a09050d8168f257ee430fedbfd21b21d52
Tyler Correll <corrett10@gmail.com>
2023-04-04 13:41:40 -0500

    login and register done

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,31 @@
+from flask import render_template,redirect,session,request, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/register',methods=['POST'])
+def register():
+    if not User.validate_register(request.form):
+        return redirect('/')
+    data ={ 
+        "username": request.form['username'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.save(data)
+    session['user_id'] = id
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("dashboard.html",user=User.get_user_by_id(data))
---
+++ flask_app/models/user.py
@@ -0,0 +1,48 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+import re	# the regex module
+# create a regular expression object that we'll use later   
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flash
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.username = data['username']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO users (username,email,password) VALUES(%(username)s,%(email)s,%(password)s)"
+        return connectToMySQL('social_climber').query_db(query,data)
+
+    @classmethod
+    def get_user_by_id(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL('social_climber').query_db(query,data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @staticmethod
+    def validate_register(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid Email","register")
+            is_valid=False
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL('social_climber').query_db(query,user)
+        if len(results) != 0:
+            flash("* Email already exists","register")
+            is_valid=False
+        if len(user['username']) < 3:
+            flash("Username must be at least 3 characters","register")
+            is_valid= False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters","register")
+            is_valid= False
+        if user['password'] != user['confirm']:
+            flash("Passwords don't match","register")
+        return is_valid
